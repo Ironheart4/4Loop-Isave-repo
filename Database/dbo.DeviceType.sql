932b2ea0f676a297d36e2b4cc211a39e9de6a7df
Ironheart4 <104942053+Ironheart4@users.noreply.github.com>
2025-08-26 12:47:26 +0200

    Auto commit on 26/08/2025 12:47:25.57

---
+++ Database/dbo.DeviceType.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DeviceType] (
+    [DeviceTypeID]      INT             IDENTITY (1, 1) NOT NULL,
+    [Name]              NVARCHAR (100)  NOT NULL,
+    [Category]          NVARCHAR (50)   NULL,
+    [TypicalPowerW]     INT             NULL,
+    [TypicalUsageHours] DECIMAL (10, 2) NULL,
+    PRIMARY KEY CLUSTERED ([DeviceTypeID] ASC)
+);
+
