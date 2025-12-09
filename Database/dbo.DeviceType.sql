289dc3b2de365df56a9f5f390c11ee9cd17fdb0a
AA Oduwole <104942053+Ironheart4@users.noreply.github.com>
2025-12-09 13:15:50 +0200

    Delete Database directory
    delete

--- Database/dbo.DeviceType.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[DeviceType] (
-    [DeviceTypeID]      INT             IDENTITY (1, 1) NOT NULL,
-    [Name]              NVARCHAR (100)  NOT NULL,
-    [Category]          NVARCHAR (50)   NULL,
-    [TypicalPowerW]     INT             NULL,
-    [TypicalUsageHours] DECIMAL (10, 2) NULL,
-    PRIMARY KEY CLUSTERED ([DeviceTypeID] ASC)
-);
-
